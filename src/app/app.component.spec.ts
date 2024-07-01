affaa1bfb5def2de4cdf95d58cc4a65f11db0f68
leandroltfx <leandrobsb55@hotmail.com>
2024-07-01 08:25:09 -0300

    iniciando feature de login

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,16 +21,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'finance-control-front-angular'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('finance-control-front-angular');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('finance-control-front-angular app is running!');
-  });
 });
