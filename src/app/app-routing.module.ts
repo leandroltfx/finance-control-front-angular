7da1bd1682322db356566640ba8b05e54e2a229c
leandroltfx <leandroltfbr@hotmail.com>
2025-03-03 09:05:58 -0300

    criando enum de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { RoutesEnum } from './shared/enum/routes.enum';
+
 const routes: Routes = [
-  { path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule) },
-  { path: 'user-registration', loadChildren: () => import('./modules/user-registration/user-registration.module').then(m => m.UserRegistrationModule) },
-  { path: '**', redirectTo: 'login' },
+  { path: RoutesEnum.LOGIN, loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule) },
+  { path: RoutesEnum.USER_REGISTRATION, loadChildren: () => import('./modules/user-registration/user-registration.module').then(m => m.UserRegistrationModule) },
+  { path: '**', redirectTo: RoutesEnum.LOGIN },
 ];
 
 @NgModule({
