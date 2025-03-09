994b8e28fbed5d21b1da6b7c3ce820e9de083ea6
leandroltfx <leandroltfbr@hotmail.com>
2025-03-09 08:01:38 -0300

    criando service global para emitir mensagens em tela

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatSnackBarModule } from '@angular/material/snack-bar'
+
+import { MessageService } from './services/message/message.service';
+
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+
+    MatSnackBarModule,
+  ],
+  providers: [
+    MessageService,
   ]
 })
 export class CoreModule { }
