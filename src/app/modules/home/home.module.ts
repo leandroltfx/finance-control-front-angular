be6d42ae13b2539b9e353b58021ff1a4643ce8a0
leandroltfx <leandroltfbr@hotmail.com>
2025-03-10 09:14:40 -0300

    criando barra de menu principal

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { TranslateModule } from '@ngx-translate/core';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 
@@ -10,6 +16,12 @@ import { HomeRoutingModule } from './home-routing.module';
   ],
   imports: [
     CommonModule,
+
+    TranslateModule,
+
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
 
     HomeRoutingModule,
   ]
