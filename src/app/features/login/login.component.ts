4c8f3d008d0a9d378a690980cf2c091b3507fe7d
leandroltfx <leandroltfbr@hotmail.com>
2025-04-03 08:46:49 -0300

    feat: criando layout da tela de login

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'fc-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent { }
+export class LoginComponent implements OnInit {
+
+  public loginForm!: FormGroup;
+  public hidePassword: boolean = true;
+  
+  constructor(
+    private readonly _formBuilder: FormBuilder,
+  ) { }
+
+  public ngOnInit(): void {
+    this.loginForm = this._buildLoginForm();
+  }
+
+  public login(): void { }
+
+  private _buildLoginForm(): FormGroup {
+    return this._formBuilder.group(
+      {
+        email: ['', [Validators.required]],
+        password: ['', [Validators.required]]
+      }
+    );
+  }
+
+}
