e5c52acc022b823f5f7cba34357ee7d36d53bfc4
leandroltfx <leandroltfbr@hotmail.com>
2025-04-04 08:26:22 -0300

    feat: criando formulário de cadastro de usuário

--- src/app/features/user-registration/user-registration.component.spec.ts
+++ src/app/features/user-registration/user-registration.component.spec.ts
@@ -1,4 +1,11 @@
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { UserRegistrationComponent } from './user-registration.component';
 
@@ -10,6 +17,15 @@ describe('UserRegistrationComponent', () => {
     TestBed.configureTestingModule({
       declarations: [
         UserRegistrationComponent
+      ],
+      imports: [
+        MatCardModule,
+        MatIconModule,
+        MatInputModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule
       ]
     });
     fixture = TestBed.createComponent(UserRegistrationComponent);
--- src/app/features/user-registration/user-registration.component.ts
+++ src/app/features/user-registration/user-registration.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { RoutesEnum } from '../../shared/enum/routes.enum';
 
 @Component({
   selector: 'fc-user-registration',
   templateUrl: './user-registration.component.html',
   styleUrls: ['./user-registration.component.css']
 })
-export class UserRegistrationComponent {
+export class UserRegistrationComponent implements OnInit {
+
+  public userRegistrationForm!: FormGroup;
+  public hidePassword: boolean = true;
+
+  private _patternEmail: RegExp = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+  private _patternUsername: RegExp = /^\S+$/;
+  private _minLengthPassword: number = 8;
+  private _minLengthUsername: number = 3;
+
+  constructor(
+    private readonly _router: Router,
+    private readonly _formBuilder: FormBuilder
+  ) { }
+
+  public ngOnInit(): void {
+    this.userRegistrationForm = this._buildUserRegistrationForm();
+  }
+
+  public registerUser(): void { }
+
+  public cancelRegisterUser(): void {
+    this._router.navigate([RoutesEnum.LOGIN]);
+  }
+
+  private _buildUserRegistrationForm(): FormGroup {
+    return this._formBuilder.group(
+      {
+        email: ['', [Validators.required, Validators.pattern(this._patternEmail)]],
+        username: ['', [Validators.required, Validators.pattern(this._patternUsername), Validators.minLength(this._minLengthUsername)]],
+        password: ['', [Validators.required, Validators.minLength(this._minLengthPassword)]]
+      }
+    );
+  }
 
 }
