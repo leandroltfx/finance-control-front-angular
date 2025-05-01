204294366fbe1d6472bf79027acf3334842403fd
leandroltfx <leandroltfbr@hotmail.com>
2025-05-01 10:03:10 -0300

    indentando código

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    imports: [
+      RouterTestingModule
+    ],
+    declarations: [
+      AppComponent
+    ]
   }));
 
   it('should create the app', () => {
