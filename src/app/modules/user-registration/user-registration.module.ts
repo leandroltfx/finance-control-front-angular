8a53267e53d27f3b755d0fa89a1364a8df10c27e
leandroltfx <leandroltfbr@hotmail.com>
2025-05-03 21:43:15 -0300

    iniciando módulo de cadastro de usuário

---
+++ src/app/modules/user-registration/user-registration.module.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+
+import { NgModule } from '@angular/core';
+
+import { UserRegistrationComponent } from './user-registration.component';
+import { UserRegistrationRoutingModule } from './user-registration-routing.module';
+
+@NgModule({
+  declarations: [
+    UserRegistrationComponent
+  ],
+  imports: [
+    CommonModule,
+
+    UserRegistrationRoutingModule
+  ]
+})
+export class UserRegistrationModule { }
