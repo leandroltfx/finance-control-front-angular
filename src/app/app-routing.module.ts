03970ed7fb0d47c200cded858570e46c01d33e86
leandroltfx <leandroltfbr@hotmail.com>
2025-05-05 08:39:07 -0300

    iniciando módulo de redefinição de senha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule) },
   { path: 'user-registration', loadChildren: () => import('./modules/user-registration/user-registration.module').then(m => m.UserRegistrationModule) },
+  { path: 'reset-password', loadChildren: () => import('./modules/reset-password/reset-password.module').then(m => m.ResetPasswordModule) },
   { path: '**', redirectTo: 'login' }
 ];
 
