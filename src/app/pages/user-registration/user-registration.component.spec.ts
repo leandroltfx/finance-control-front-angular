4c8e3c70434e424cfb9452c48582da6684acd871
leandroltfx <leandroltfbr@hotmail.com>
2025-06-01 18:57:39 -0300

    criando formulário de cadastro de usuário

--- src/app/pages/user-registration/user-registration.component.spec.ts
+++ src/app/pages/user-registration/user-registration.component.spec.ts
@@ -1,4 +1,6 @@
+import { Validators } from '@angular/forms';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { UserRegistrationComponent } from './user-registration.component';
 
@@ -8,7 +10,12 @@ describe('UserRegistrationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UserRegistrationComponent]
+      imports: [
+        UserRegistrationComponent
+      ],
+      providers: [
+        provideAnimationsAsync()
+      ]
     })
       .compileComponents();
 
@@ -19,5 +26,203 @@ describe('UserRegistrationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('registerUser', () => {
+
+    it('deve chamar o serviço de cadastro de usuário se o formulário estiver válido', () => {
+
+      const logSpy = spyOn(console, 'log');
+
+      component.userRegistrationForm = component['_buildUserRegistrationForm']();
+
+      component.userRegistrationForm.controls['username'].setValue('admin');
+      component.userRegistrationForm.controls['email'].setValue('admin@mail.com');
+      component.userRegistrationForm.controls['password'].setValue('admin12345');
+
+      component.registerUser();
+
+      expect(logSpy).toHaveBeenCalledWith('chamar o serviço de cadastro de usuário');
+    });
+
+    it('não deve chamar o serviço de cadastro de usuário se o nome de usuário não for preenchido', () => {
+
+      const logSpy = spyOn(console, 'log');
+
+      component.userRegistrationForm = component['_buildUserRegistrationForm']();
+
+      component.userRegistrationForm.controls['username'].setValue('');
+      component.userRegistrationForm.controls['email'].setValue('admin@mail.com');
+      component.userRegistrationForm.controls['password'].setValue('admin12345');
+
+      component.registerUser();
+
+      expect(logSpy).not.toHaveBeenCalled();
+    });
+
+    it('não deve chamar o serviço de cadastro de usuário se o nome de usuário estiver com espaços em branco no meio', () => {
+
+      const logSpy = spyOn(console, 'log');
+
+      component.userRegistrationForm = component['_buildUserRegistrationForm']();
+
+      component.userRegistrationForm.controls['username'].setValue('ad min');
+      component.userRegistrationForm.controls['email'].setValue('admin@mail.com');
+      component.userRegistrationForm.controls['password'].setValue('admin12345');
+
+      component.registerUser();
+
+      expect(logSpy).not.toHaveBeenCalled();
+    });
+
+    it('não deve chamar o serviço de cadastro de usuário se o nome de usuário estiver com espaços em branco no início', () => {
+
+      const logSpy = spyOn(console, 'log');
+
+      component.userRegistrationForm = component['_buildUserRegistrationForm']();
+
+      component.userRegistrationForm.controls['username'].setValue(' admin');
+      component.userRegistrationForm.controls['email'].setValue('admin@mail.com');
+      component.userRegistrationForm.controls['password'].setValue('admin12345');
+
+      component.registerUser();
+
+      expect(logSpy).not.toHaveBeenCalled();
+    });
+
+    it('não deve chamar o serviço de cadastro de usuário se o nome de usuário estiver com espaços em branco no fim', () => {
+
+      const logSpy = spyOn(console, 'log');
+
+      component.userRegistrationForm = component['_buildUserRegistrationForm']();
+
+      component.userRegistrationForm.controls['username'].setValue('admin ');
+      component.userRegistrationForm.controls['email'].setValue('admin@mail.com');
+      component.userRegistrationForm.controls['password'].setValue('admin12345');
+
+      component.registerUser();
+
+      expect(logSpy).not.toHaveBeenCalled();
+    });
+
+    it('não deve chamar o serviço de cadastro de usuário se o nome de usuário estiver com caracteres especiais', () => {
+
+      const logSpy = spyOn(console, 'log');
+
+      component.userRegistrationForm = component['_buildUserRegistrationForm']();
+
+      component.userRegistrationForm.controls['username'].setValue('ad#min');
+      component.userRegistrationForm.controls['email'].setValue('admin@mail.com');
+      component.userRegistrationForm.controls['password'].setValue('admin12345');
+
+      component.registerUser();
+
+      expect(logSpy).not.toHaveBeenCalled();
+    });
+
+    it('não deve chamar o serviço de cadastro de usuário se o nome de usuário estiver com apenas números', () => {
+
+      const logSpy = spyOn(console, 'log');
+
+      component.userRegistrationForm = component['_buildUserRegistrationForm']();
+
+      component.userRegistrationForm.controls['username'].setValue('12313231');
+      component.userRegistrationForm.controls['email'].setValue('admin@mail.com');
+      component.userRegistrationForm.controls['password'].setValue('admin12345');
+
+      component.registerUser();
+
+      expect(logSpy).not.toHaveBeenCalled();
+    });
+
+    it('não deve chamar o serviço de cadastro de usuário se o nome de usuário iniciar com número', () => {
+
+      const logSpy = spyOn(console, 'log');
+
+      component.userRegistrationForm = component['_buildUserRegistrationForm']();
+
+      component.userRegistrationForm.controls['username'].setValue('1asd123asd');
+      component.userRegistrationForm.controls['email'].setValue('admin@mail.com');
+      component.userRegistrationForm.controls['password'].setValue('admin12345');
+
+      component.registerUser();
+
+      expect(logSpy).not.toHaveBeenCalled();
+    });
+
+    it('não deve chamar o serviço de cadastro de usuário se o email não for preenchido', () => {
+
+      const logSpy = spyOn(console, 'log');
+
+      component.userRegistrationForm = component['_buildUserRegistrationForm']();
+
+      component.userRegistrationForm.controls['username'].setValue('username');
+      component.userRegistrationForm.controls['email'].setValue('');
+      component.userRegistrationForm.controls['password'].setValue('admin12345');
+
+      component.registerUser();
+
+      expect(logSpy).not.toHaveBeenCalled();
+    });
+
+    it('não deve chamar o serviço de cadastro de usuário se o email for inválido', () => {
+
+      const logSpy = spyOn(console, 'log');
+
+      component.userRegistrationForm = component['_buildUserRegistrationForm']();
+
+      component.userRegistrationForm.controls['username'].setValue('username');
+      component.userRegistrationForm.controls['email'].setValue('usernameemail.com');
+      component.userRegistrationForm.controls['password'].setValue('admin12345');
+
+      component.registerUser();
+
+      expect(logSpy).not.toHaveBeenCalled();
+    });
+
+    it('não deve chamar o serviço de cadastro de usuário se a senha não for preenchida', () => {
+
+      const logSpy = spyOn(console, 'log');
+
+      component.userRegistrationForm = component['_buildUserRegistrationForm']();
+
+      component.userRegistrationForm.controls['username'].setValue('username');
+      component.userRegistrationForm.controls['email'].setValue('admin@mail.com');
+      component.userRegistrationForm.controls['password'].setValue('');
+
+      component.registerUser();
+
+      expect(logSpy).not.toHaveBeenCalled();
+    });
+
+    it('não deve chamar o serviço de cadastro de usuário se a senha não conter no mínimo 8 caracteres', () => {
+
+      const logSpy = spyOn(console, 'log');
+
+      component.userRegistrationForm = component['_buildUserRegistrationForm']();
+
+      component.userRegistrationForm.controls['username'].setValue('username');
+      component.userRegistrationForm.controls['email'].setValue('admin@mail.com');
+      component.userRegistrationForm.controls['password'].setValue('admin12');
+
+      component.registerUser();
+
+      expect(logSpy).not.toHaveBeenCalled();
+    });
+  });
+
+  it('deve montar o formulário de cadastro de usuário', () => {
+
+    component.userRegistrationForm = component['_buildUserRegistrationForm']();
+
+    expect(component.userRegistrationForm.controls['username'].hasValidator(Validators.required)).toBeTrue();
+    expect(component.userRegistrationForm.controls['username'].hasValidator(component['_validatorPatternUsername'])).toBeTrue();
+    expect(component.userRegistrationForm.controls['username'].hasValidator(component['_validatorMinLengthUsername'])).toBeTrue();
+
+    expect(component.userRegistrationForm.controls['email'].hasValidator(Validators.required)).toBeTrue();
+    expect(component.userRegistrationForm.controls['email'].hasValidator(component['_validatorPatternEmail'])).toBeTrue();
+
+    expect(component.userRegistrationForm.controls['password'].hasValidator(Validators.required)).toBeTrue();
+    expect(component.userRegistrationForm.controls['password'].hasValidator(component['_validatorMinLengthPassword'])).toBeTrue();
   });
 });
--- src/app/pages/user-registration/user-registration.component.ts
+++ src/app/pages/user-registration/user-registration.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { FormBuilder, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'fc-user-registration',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatFormFieldModule
+  ],
   templateUrl: './user-registration.component.html',
-  styleUrl: './user-registration.component.css'
+  styleUrls: ['./user-registration.component.css']
 })
-export class UserRegistrationComponent { }
+export class UserRegistrationComponent implements OnInit {
+
+  public userRegistrationForm!: FormGroup;
+  public hidePassword: boolean = true;
+
+  private _validatorMinLengthUsername: ValidatorFn = Validators.minLength(3);
+  private _validatorMinLengthPassword: ValidatorFn = Validators.minLength(8);
+  private _validatorPatternUsername: ValidatorFn = Validators.pattern(/^[a-zA-Z][a-zA-Z0-9]*$/);
+  private _validatorPatternEmail: ValidatorFn = Validators.pattern(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i);
+
+  constructor(
+    private readonly _formBuilder: FormBuilder
+  ) { }
+
+  public ngOnInit(): void {
+    this.userRegistrationForm = this._buildUserRegistrationForm();
+  }
+
+  public registerUser(): void {
+    if (this.userRegistrationForm.valid) {
+      console.log('chamar o serviço de cadastro de usuário');
+    }
+  }
+
+  private _buildUserRegistrationForm(): FormGroup {
+    return this._formBuilder.group(
+      {
+        username: [null, [Validators.required, this._validatorMinLengthUsername, this._validatorPatternUsername]],
+        email: [null, [Validators.required, this._validatorPatternEmail]],
+        password: [null, [Validators.required, this._validatorMinLengthPassword]]
+      }
+    );
+  }
+
+}
